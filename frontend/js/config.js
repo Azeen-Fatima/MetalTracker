99ddcfa7b5c6104f4d63bebeb2dfc6b01c53462f
Azeen-Fatima <azeenfatima283@gmail.com>
2026-02-23 00:34:40 +0500

    Changed

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,4 +2,4 @@ const API_BASE_URL = (
     window.location.hostname === 'localhost' ||
     window.location.hostname === '127.0.0.1' ||
     window.location.hostname === ''
-) ? 'http://localhost:5000' : 'https://metaltracker-production-09fa.up.railway.app';
+) ? 'http://localhost:5000' : 'https://metaltracker.onrender.com';
